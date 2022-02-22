21adea81847a35fc634c68bab239bc2bf03c7e96
Mohamad Bagheri <92805733+mohammadbagheri93@users.noreply.github.com>
2022-02-22 06:47:52 -0800

    Add files via upload
    first upload

---
+++ aqau-vol.cal.py
@@ -0,0 +1,14 @@
+
+try :
+    LENGHT = int(input("enter lenght aquarium"))
+    HEIGHT = int(input("enter height aquarium"))
+    DEPTH = int(input("enter depth aquarium"))
+    volume = (LENGHT * HEIGHT * DEPTH) / 1000
+except :
+    print("somthing went wrong!\nplease enter a number")    
+
+else :
+    print("thise aquariume volume is %d L" %(volume))
+
+
+
