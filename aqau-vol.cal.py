db83a8fd13e3edc66bcbb42039a7cf62d2e61154
mohammadbagheri93 <m.bagheri11993@gmail.com>
2022-02-23 08:37:54 -0800

    add volumes

--- aqau-vol.cal.py
+++ aqau-vol.cal.py
@@ -2,13 +2,23 @@
 try :
     LENGHT = int(input("enter lenght aquarium"))
     HEIGHT = int(input("enter height aquarium"))
-    DEPTH = int(input("enter depth aquarium"))
-    volume = (LENGHT * HEIGHT * DEPTH) / 1000
+    WIDTH = int(input("enter depth aquarium"))
+    volume = (LENGHT * HEIGHT * WIDTH) / 1000
+    volume_square = (((HEIGHT * LENGHT / 100) * 2) + ((HEIGHT * WIDTH / 100) * 2) + (WIDTH * LENGHT / 100))
 except :
     print("somthing went wrong!\nplease enter a number")    
 
 else :
-    print("thise aquariume volume is %d litres." %(volume))
+    print("""thise aquariume volume is %d litres.
+    Size of sides glasses is :
+    {HEIGHT} * {LENGHT}
+    {HEIGHT} * {LENGHT} 
+    {HEIGHT} * {WIDTH} 
+    {HEIGHT} * {WIDTH}
+    {WIDTH} * {LENGHT} 
+    Total square meter is :
+    {volume_square}
+    """ %(volume))
 
 
 
